c9cd0625447438cd5412fa649e86a3dedddb07b3
Kaynir <kaynir.games@gmail.com>
2022-12-08 20:23:44 +0300

    Проверка при выгрузке сцены.

--- Runtime/Scripts/SceneLoader.cs
+++ Runtime/Scripts/SceneLoader.cs
@@ -15,7 +15,7 @@ namespace Kaynir.Scenes
         public void LoadSceneFaded(int activeScene, List<int> extraScenes)
         {
             if (IsLoading) return;
-            StartCoroutine(LoadRoutine(activeScene, extraScenes));
+            StartCoroutine(LoadFadedRoutine(activeScene, extraScenes));
         }
 
         public void LoadSceneFaded(int activeScene)
@@ -33,32 +33,50 @@ namespace Kaynir.Scenes
             LoadSceneFaded(collection.ActiveScene, collection.ExtraScenes);
         }
 
-        public AsyncOperation LoadScene(int sceneIndex, LoadSceneMode mode)
+        public void LoadScene(int sceneIndex, LoadSceneMode mode)
         {
-            return SceneManager.LoadSceneAsync(sceneIndex, mode);
+            GetLoadSceneOperation(sceneIndex, mode);
         }
 
         public void UnloadScene(int sceneIndex)
         {
-            if (!SceneManager.GetSceneByBuildIndex(sceneIndex).isLoaded) return;
-            SceneManager.UnloadSceneAsync(sceneIndex);
+            if (!IsLoadedScene(sceneIndex))
+            {
+                Debug.LogWarning($"Unload operation failed. Scene [{sceneIndex}] is not loaded.");
+                return;
+            }
+
+            GetUnloadSceneOperation(sceneIndex);
         }
 
-        private IEnumerator LoadRoutine(int activeScene, List<int> extraScenes)
+        public bool IsLoadedScene(int sceneIndex)
+        {
+            return SceneManager.GetSceneByBuildIndex(sceneIndex).isLoaded;
+        }
+
+        private AsyncOperation GetLoadSceneOperation(int sceneIndex, LoadSceneMode mode)
+        {
+            return SceneManager.LoadSceneAsync(sceneIndex, mode);
+        }
+
+        private AsyncOperation GetUnloadSceneOperation(int sceneIndex)
+        {
+            return SceneManager.UnloadSceneAsync(sceneIndex);
+        }
+
+        private IEnumerator LoadFadedRoutine(int activeScene, List<int> extraScenes)
         {
             IsLoading = true;
-
             yield return _splashScreen.FadeInRoutine();
             
-            yield return LoadScene(activeScene, LoadSceneMode.Single);
+            yield return GetLoadSceneOperation(activeScene, LoadSceneMode.Single);
 
             foreach (int scene in extraScenes)
             {
-                yield return LoadScene(scene, LoadSceneMode.Additive);
+                yield return GetLoadSceneOperation(scene, LoadSceneMode.Additive);
             }
 
             yield return _splashScreen.FadeOutRoutine();
-
             IsLoading = false;
         }
     }
